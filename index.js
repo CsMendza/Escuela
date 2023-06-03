8d2feca33568197e17b485bb6e526d8e7314e9d3
Carlos Eduardo Mendoza Juarez <cmendoza42@alumnos.uaq.mx>
2023-06-02 20:23:10 -0600

    1

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const app = express();
+const pokedex = require('.poke.json');
+
+
 
 app.get("/", (req, res, next) => {
-    res.status(200);
-    res.send("Bienvenido");
+    const pokemon = pokedex.pokemon; //el .pokemon es porque es la primera llave
+    res.send(pokemon);
 }) 
 
-app.listen(3000, ()=>{
+app.get("/:name", (req, res, next) => {  //Cuando le ponemos diagonal en el get es para dirigirnos a la siguiente pagina
+    res.status(200)                                             //req = request o peticion, el dato que nos va a estar enviando el usuario
+    console.log(req.params.name);
+    res.send("Estas en la pagina Nombre");
+});
+
+app.listen(process.env.PORT || 3000, () => { 
     console.log("Server is running...");
 });
